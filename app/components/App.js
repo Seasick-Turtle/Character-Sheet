8e7f914e02353024b926f06dab7c80c56cb96afc
Christopher Martin <martincdev@gmail.com>
2017-10-31 03:40:11 -0500

    Add home component

--- app/components/App.js
+++ app/components/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class App extends Component {
   render() {
     return(
-      <div>App Component!g</div>
+      <div>App Component</div>
     )
   }
 }
